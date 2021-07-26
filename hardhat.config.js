5581016d881f90c51df7f8ed27467ba7b8290afc
Jack Li <jackz163299@gmail.com>
2021-07-26 13:19:58 -0400

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,58 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+require("dotenv/config")
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    mumbai: {
+      url: "https://focused-bassi:shack-wafer-trade-aliens-doze-staple@nd-267-106-017.p2pify.com",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    matic: {
+      url: "https://rpc-mainnet.matic.network",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    // apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: process.env.MATIC_API_KEY,
+  },
+};
+
